b82aea1b4e0ba511053768dd4d5979b283239bcb
Frédéric Devernay <frederic.devernay@m4x.org>
2014-02-28 15:53:38 +0100

    PixelCopier: use memcpy()

--- IOSupport/ofxsCopier.h
+++ IOSupport/ofxsCopier.h
@@ -8,6 +8,8 @@
 
 #ifndef IO_ofxsCopier_h
 #define IO_ofxsCopier_h
+
+#include <cstring>
 #include "ofxsPixelProcessor.h"
 
 // Base class for the RGBA and the Alpha processor
@@ -24,19 +26,13 @@ class PixelCopier : public OFX::PixelProcessorFilterBase {
     // and do some processing
     void multiThreadProcessImages(OfxRectI procWindow)
     {
+        int rowBytes = sizeof(PIX) * nComponents * (procWindow.x2 - procWindow.x1);
         for(int y = procWindow.y1; y < procWindow.y2; y++) {
             if(_effect.abort()) break;
 
             PIX *dstPix = (PIX *) getDstPixelAddress(procWindow.x1, y);
-
-            for(int x = procWindow.x1; x < procWindow.x2; x++) {
-                const PIX *srcPix = (const PIX *) getSrcPixelAddress(x, y);
-                for(int c = 0; c < nComponents; c++) {
-                    dstPix[c] = srcPix[c];
-                }
-                // increment the dst pixel
-                dstPix += nComponents;
-            }
+            const PIX *srcPix = (const PIX *) getSrcPixelAddress(procWindow.x1, y);
+            std::memcpy(dstPix, srcPix, rowBytes);
         }
     }
 };
