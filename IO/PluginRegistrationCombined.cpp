acbbcc7f74a3158a9ef0b2b671ce759fb8aa1839
MrKepzie <alexandre230@msn.com>
2014-04-23 08:48:56 +0200

    Update the minor version of the plug-ins depending on the GenericReader

--- IO/PluginRegistrationCombined.cpp
+++ IO/PluginRegistrationCombined.cpp
@@ -18,19 +18,19 @@ namespace OFX
   {
     void getPluginIDs(OFX::PluginFactoryArray &ids)
     {
-      static ReadEXRPluginFactory p1("fr.inria.openfx:ReadEXR", 1, 0);
+      static ReadEXRPluginFactory p1("fr.inria.openfx:ReadEXR", 1, 1);
       ids.push_back(&p1);
       static WriteEXRPluginFactory p2("fr.inria.openfx:WriteEXR", 1, 0);
       ids.push_back(&p2);
-      static ReadFFmpegPluginFactory p3("fr.inria.openfx:ReadFFmpeg", 1, 0);
+      static ReadFFmpegPluginFactory p3("fr.inria.openfx:ReadFFmpeg", 1, 1);
       ids.push_back(&p3);
       static WriteFFmpegPluginFactory p4("fr.inria.openfx:WriteFFmpeg", 1, 0);
       ids.push_back(&p4);
-      static ReadOIIOPluginFactory p5("fr.inria.openfx:ReadOIIO", 1, 0);
+      static ReadOIIOPluginFactory p5("fr.inria.openfx:ReadOIIO", 1, 1);
       ids.push_back(&p5);
       static WriteOIIOPluginFactory p6("fr.inria.openfx:WriteOIIO", 1, 0);
       ids.push_back(&p6);
-      static ReadPFMPluginFactory p7("fr.inria.openfx:ReadPFM", 1, 0);
+      static ReadPFMPluginFactory p7("fr.inria.openfx:ReadPFM", 1, 1);
       ids.push_back(&p7);
       static WritePFMPluginFactory p8("fr.inria.openfx:WritePFM", 1, 0);
       ids.push_back(&p8);
