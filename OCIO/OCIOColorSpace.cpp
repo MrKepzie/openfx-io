b34bf6772069818c897502f5fd09ceb1dd8e5916
Frédéric Devernay <frederic.devernay@m4x.org>
2014-07-24 15:18:31 +0200

    OCIOColorSpace: format

--- OCIO/OCIOColorSpace.cpp
+++ OCIO/OCIOColorSpace.cpp
@@ -1,24 +1,24 @@
 /*
  OCIOColorSpace plugin.
  Convert from one colorspace to another.
- 
+
  Copyright (C) 2014 INRIA
  Author: Frederic Devernay <frederic.devernay@inria.fr>
- 
+
  Redistribution and use in source and binary forms, with or without modification,
  are permitted provided that the following conditions are met:
- 
+
  Redistributions of source code must retain the above copyright notice, this
  list of conditions and the following disclaimer.
- 
+
  Redistributions in binary form must reproduce the above copyright notice, this
  list of conditions and the following disclaimer in the documentation and/or
  other materials provided with the distribution.
- 
+
  Neither the name of the {organization} nor the names of its
  contributors may be used to endorse or promote products derived from
  this software without specific prior written permission.
- 
+
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -29,12 +29,12 @@
  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
+
  INRIA
  Domaine de Voluceau
  Rocquencourt - B.P. 105
  78153 Le Chesnay Cedex - France
- 
+
  */
 
 
@@ -55,33 +55,33 @@
 #define kPluginVersionMajor 1 // Incrementing this number means that you have broken backwards compatibility of the plug-in.
 #define kPluginVersionMinor 0 // Increment this when you have fixed a bug or made it faster.
 
-class OCIOColorSpacePlugin : public OFX::ImageEffect 
+class OCIOColorSpacePlugin : public OFX::ImageEffect
 {
 public:
-    
+
     OCIOColorSpacePlugin(OfxImageEffectHandle handle);
-    
+
     virtual ~OCIOColorSpacePlugin();
-    
-  /* Override the render */
-  virtual void render(const OFX::RenderArguments &args);
 
-  /* override is identity */
-  virtual bool isIdentity(const OFX::RenderArguments &args, OFX::Clip * &identityClip, double &identityTime);
+    /* Override the render */
+    virtual void render(const OFX::RenderArguments &args);
 
-  /* override changedParam */
-  virtual void changedParam(const OFX::InstanceChangedArgs &args, const std::string &paramName);
+    /* override is identity */
+    virtual bool isIdentity(const OFX::RenderArguments &args, OFX::Clip * &identityClip, double &identityTime);
 
-  /* override changed clip */
-  //virtual void changedClip(const OFX::InstanceChangedArgs &args, const std::string &clipName);
+    /* override changedParam */
+    virtual void changedParam(const OFX::InstanceChangedArgs &args, const std::string &paramName);
 
-  // override the rod call
-  //virtual bool getRegionOfDefinition(const OFX::RegionOfDefinitionArguments &args, OfxRectD &rod);
+    /* override changed clip */
+    //virtual void changedClip(const OFX::InstanceChangedArgs &args, const std::string &clipName);
 
-  // override the roi call
-  //virtual void getRegionsOfInterest(const OFX::RegionsOfInterestArguments &args, OFX::RegionOfInterestSetter &rois);
+    // override the rod call
+    //virtual bool getRegionOfDefinition(const OFX::RegionOfDefinitionArguments &args, OfxRectD &rod);
 
-private :
+    // override the roi call
+    //virtual void getRegionsOfInterest(const OFX::RegionsOfInterestArguments &args, OFX::RegionOfInterestSetter &rois);
+
+private:
     void copyPixelData(const OfxRectI &renderWindow,
                        const OFX::Image* srcImg,
                        OFX::Image* dstImg)
@@ -153,11 +153,11 @@ private :
                        OFX::BitDepthEnum dstBitDepth,
                        int dstRowBytes);
 
-  // do not need to delete these, the ImageEffect is managing them for us
-  OFX::Clip *dstClip_;
-  OFX::Clip *srcClip_;
+    // do not need to delete these, the ImageEffect is managing them for us
+    OFX::Clip *dstClip_;
+    OFX::Clip *srcClip_;
 
-  GenericOCIO* _ocio;
+    GenericOCIO* _ocio;
 };
 
 OCIOColorSpacePlugin::OCIOColorSpacePlugin(OfxImageEffectHandle handle)
@@ -172,7 +172,6 @@ OCIOColorSpacePlugin::OCIOColorSpacePlugin(OfxImageEffectHandle handle)
 
 OCIOColorSpacePlugin::~OCIOColorSpacePlugin()
 {
-    
 }
 
 /* set up and run a copy processor */
