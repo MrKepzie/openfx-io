def4ce473ec420493fad3431b411590309aacf69
Frédéric Devernay <frederic.devernay@m4x.org>
2014-09-02 16:24:34 +0200

    PluginRegistrationCombined.cpp : clean up

--- IO/PluginRegistrationCombined.cpp
+++ IO/PluginRegistrationCombined.cpp
@@ -1,21 +1,23 @@
 #include "ofxsImageEffect.h"
+#ifdef OFX_IO_USE_DEPRECATED_EXR
 #include "ReadEXR.h"
 #include "WriteEXR.h"
+#endif
 #include "ReadFFmpeg.h"
 #include "WriteFFmpeg.h"
+#include "ReadPFM.h"
+#include "WritePFM.h"
 #include "ReadOIIO.h"
 #include "WriteOIIO.h"
+#include "OIIOText.h"
+#include "OIIOResize.h"
+#ifdef OFX_IO_USING_OCIO
 #include "OCIOCDLTransform.h"
 #include "OCIOColorSpace.h"
 #include "OCIOFileTransform.h"
 #include "OCIOLogConvert.h"
-#ifdef DEBUG
-#include "OIIOText.h"
-#include "OIIOResize.h"
 #endif
-#include "ReadPFM.h"
-#include "WritePFM.h"
-#ifndef _WIN32
+#ifndef _WINDOWS
 #include "RunScript.h"
 #endif
 
@@ -25,25 +27,37 @@ namespace OFX
     {
         void getPluginIDs(OFX::PluginFactoryArray &ids)
         {
-            //      static ReadEXRPluginFactory p1("fr.inria.openfx:ReadEXR", 1, 1);
-            //      ids.push_back(&p1);
-            //      static WriteEXRPluginFactory p2("fr.inria.openfx:WriteEXR", 1, 0);
-            //      ids.push_back(&p2);
-            static ReadFFmpegPluginFactory p3("fr.inria.openfx:ReadFFmpeg", 1, 1);
-            ids.push_back(&p3);
-            static WriteFFmpegPluginFactory p4("fr.inria.openfx:WriteFFmpeg", 1, 0);
-            ids.push_back(&p4);
+#ifdef OFX_IO_USE_DEPRECATED_EXR
+            // EXR plugins are deprecated and should be revised/rewritten
+            {
+                static ReadEXRPluginFactory p("fr.inria.openfx:ReadEXR", 1, 1);
+                ids.push_back(&p);
+            }
+            {
+                static WriteEXRPluginFactory p("fr.inria.openfx:WriteEXR", 1, 0);
+                ids.push_back(&p);
+            }
+#endif
+            {
+                static ReadFFmpegPluginFactory p("fr.inria.openfx:ReadFFmpeg", 1, 1);
+                ids.push_back(&p);
+            }
+            {
+                static WriteFFmpegPluginFactory p("fr.inria.openfx:WriteFFmpeg", 1, 0);
+                ids.push_back(&p);
+            }
+            {
+                static ReadPFMPluginFactory p("fr.inria.openfx:ReadPFM", 1, 1);
+                ids.push_back(&p);
+            }
+            {
+                static WritePFMPluginFactory p("fr.inria.openfx:WritePFM", 1, 0);
+                ids.push_back(&p);
+            }
             getReadOIIOPluginID(ids);
             getWriteOIIOPluginID(ids);
-#ifdef DEBUG
-            getOIIOTextPluginID(ids); //< not ready
-#endif
+            getOIIOTextPluginID(ids);
             getOIIOResizePluginID(ids);
-
-            static ReadPFMPluginFactory p7("fr.inria.openfx:ReadPFM", 1, 1);
-            ids.push_back(&p7);
-            static WritePFMPluginFactory p8("fr.inria.openfx:WritePFM", 1, 0);
-            ids.push_back(&p8);
 #ifdef OFX_IO_USING_OCIO
             getOCIOCDLTransformPluginID(ids);
             getOCIOColorSpacePluginID(ids);
