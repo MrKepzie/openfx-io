0156e6343658883af4b25bbb34b1306dc1647cc9
Frédéric Devernay <frederic.devernay@m4x.org>
2014-12-01 10:11:02 +0100

    fix warnings

--- IOSupport/IOUtility.h
+++ IOSupport/IOUtility.h
@@ -41,19 +41,9 @@
 #define IO_Utility_h
 
 
-// compiler_warning.h
-#define STRINGISE_IMPL(x) #x
-#define STRINGISE(x) STRINGISE_IMPL(x)
-
-// Use: #pragma message WARN("My message")
-#if _MSC_VER
-#   define FILE_LINE_LINK __FILE__ "(" STRINGISE(__LINE__) ") : "
-#   define WARN(exp) (FILE_LINE_LINK "WARNING: " exp)
-#else//__GNUC__ - may need other defines for different compilers
-#   define WARN(exp) ("WARNING: " exp)
-#endif
-
 #include <cmath>
+#include <cassert>
+#include <algorithm>
 #include "ofxsImageEffect.h"
 
 /// numvals should be 256 for byte, 65536 for 16-bits, etc.
