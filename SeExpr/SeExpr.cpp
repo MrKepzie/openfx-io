1fe889a69248c79ddd6df2701813654b9b364422
Alexandre Gauthier <alexandre230@msn.com>
2015-03-16 18:27:10 +0100

    SeExpr: add Size/Format/Project Region of Definition choices

--- SeExpr/SeExpr.cpp
+++ SeExpr/SeExpr.cpp
@@ -85,6 +85,9 @@
 #include "ofxsCopier.h"
 #include "ofxsMerging.h"
 #include "ofxsMultiThread.h"
+#include "ofxsFormatResolution.h"
+#include "ofxsGenerator.h"
+#include "ofxsRectangleInteract.h"
 
 #include <SeExpression.h>
 #include <SeExprFunc.h>
@@ -177,12 +180,22 @@
 #define kParamRegionOfDefinitionLabel "Region of Definition"
 #define kParamRegionOfDefinitionHint "The region of definition of the output."
 
+#define kParamRegionOfDefinitionOptionFormat "Format"
+#define kParamRegionOfDefinitionOptionFormatHelp "The output region will be of the specified format."
+#define kParamRegionOfDefinitionOptionProject "Project"
+#define kParamRegionOfDefinitionOptionProjectHelp "The output region will be of the size of the project."
+#define kParamRegionOfDefinitionOptionSize "Size"
+#define kParamRegionOfDefinitionOptionSizeHelp "The output region will be of the size of the rectangle overlay."
 #define kParamRegionOfDefinitionOptionUnion "Union"
 #define kParamRegionOfDefinitionOptionUnionHelp "The output region will be the union of the regions of definition of all connected inputs."
 #define kParamRegionOfDefinitionOptionIntersection "Intersection"
 #define kParamRegionOfDefinitionOptionIntersectionHelp "The output region will be the intersection the regions of definition of all connected inputs."
 #define kParamRegionOfDefinitionOptionCustomInput "Input%d"
 #define kParamRegionOfDefinitionOptionCustomInputHelp "The output region will be the regions of definition of input %d."
+
+#define kParamGeneratorFormat "format"
+#define kParamGeneratorFormatLabel "Format"
+#define kParamGeneratorFormatHint "The output format"
 
 #define kParamLayerInput "layerInput%d"
 #define kParamLayerInputLabel "Input Layer %d"
@@ -303,6 +316,12 @@ private:
     OFX::BooleanParam* _maskInvert;
     
     OFX::ChoiceParam* _boundingBox;
+    
+    OFX::ChoiceParam* _format;
+    OFX::Double2DParam* _btmLeft;
+    OFX::Double2DParam* _size;
+    OFX::BooleanParam* _interactive;
+
 };
 
 class OFXSeExpression;
@@ -1320,6 +1339,13 @@ SeExprPlugin::SeExprPlugin(OfxImageEffectHandle handle)
     _boundingBox = fetchChoiceParam(kParamRegionOfDefinition);
     assert(_boundingBox);
 
+    
+    _format = fetchChoiceParam(kParamGeneratorFormat);
+    _btmLeft = fetchDouble2DParam(kParamRectangleInteractBtmLeft);
+    _size = fetchDouble2DParam(kParamRectangleInteractSize);
+    _interactive = fetchBooleanParam(kParamRectangleInteractInteractive);
+    assert(_format && _btmLeft && _size && _interactive);
+    
     // update visibility
     int numVisible;
     _doubleParamCount->getValue(numVisible);
@@ -1340,6 +1366,20 @@ SeExprPlugin::SeExprPlugin(OfxImageEffectHandle handle)
         bool visible = i < numVisible;
         _colorParams[i]->setIsSecret(!visible);
     }
+
+    int boundingbox_i;
+    _boundingBox->getValue(boundingbox_i);
+    bool hasFormat = (boundingbox_i == 3);
+    bool hasSize = (boundingbox_i == 2);
+    
+    _format->setEnabled(hasFormat);
+    _format->setIsSecret(!hasFormat);
+    _size->setEnabled(hasSize);
+    _size->setIsSecret(!hasSize);
+    _btmLeft->setEnabled(hasSize);
+    _btmLeft->setIsSecret(!hasSize);
+    _interactive->setEnabled(hasSize);
+    _interactive->setIsSecret(!hasSize);
 }
 
 std::string
@@ -1637,6 +1677,20 @@ SeExprPlugin::changedParam(const OFX::InstanceChangedArgs &args, const std::stri
         if (validated) {
             clearPersistentMessage();
         }
+    } else if (paramName == kParamRegionOfDefinition && args.reason == OFX::eChangeUserEdit) {
+        int boundingbox_i;
+        _boundingBox->getValue(boundingbox_i);
+        bool hasFormat = (boundingbox_i == 3);
+        bool hasSize = (boundingbox_i == 2);
+        
+        _format->setEnabled(hasFormat);
+        _format->setIsSecret(!hasFormat);
+        _size->setEnabled(hasSize);
+        _size->setIsSecret(!hasSize);
+        _btmLeft->setEnabled(hasSize);
+        _btmLeft->setIsSecret(!hasSize);
+        _interactive->setEnabled(hasSize);
+        _interactive->setIsSecret(!hasSize);
     }
 
 }
@@ -1700,6 +1754,36 @@ SeExprPlugin::getClipPreferences(OFX::ClipPreferencesSetter &clipPreferences)
 {
     if (gHostIsMultiPlanar) {
         buildChannelMenus();
+    }
+    
+    
+    double par = 0.;
+    
+    int boundingBox_i;
+    _boundingBox->getValue(boundingBox_i);
+
+    
+    if (boundingBox_i == 2) {
+        //size
+    } else if (boundingBox_i == 3) {
+        //format
+        int index;
+        _format->getValue(index);
+        size_t w,h;
+        getFormatResolution( (OFX::EParamFormat)index, &w, &h, &par );
+    } else if (boundingBox_i == 4) {
+        //project format
+        
+        /// this should be the defalut value given by the host, no need to set it.
+        /// @see Instance::setupClipPreferencesArgs() in HostSupport, it should have
+        /// the line:
+        /// double inputPar = getProjectPixelAspectRatio();
+        
+        //par = getProjectPixelAspectRatio();
+    }
+    
+    if (par != 0.) {
+        clipPreferences.setPixelAspectRatio(*_dstClip, par);
     }
 }
 
@@ -1885,6 +1969,36 @@ SeExprPlugin::getRegionOfDefinition(const OFX::RegionOfDefinitionArguments &args
                 rodSet = true;
             }
         }
+    } else if (boundingBox_i == 2) {
+      // custom size
+        _size->getValue(rod.x2, rod.y2);
+        _btmLeft->getValue(rod.x1, rod.y1);
+        rod.x2 += rod.x1;
+        rod.y2 += rod.y1;
+        
+        rodSet = true;
+    } else if (boundingBox_i == 3) {
+      // format
+        
+        int format_i;
+        _format->getValue(format_i);
+        double par;
+        size_t w,h;
+        getFormatResolution( (OFX::EParamFormat)format_i, &w, &h, &par );
+        rod.x1 = rod.y1 = 0;
+        rod.x2 = w;
+        rod.y2 = h;
+        
+        rodSet = true;
+    } else if (boundingBox_i == 4) {
+      // project
+        OfxPointD extent = getProjectExtent();
+        OfxPointD offset = getProjectOffset();
+        rod.x1 = offset.x;
+        rod.y1 = offset.y;
+        rod.x2 = extent.x;
+        rod.y2 = extent.y;
+        rodSet =true;
     } else {
         int inputIndex = boundingBox_i - 2;
         assert(inputIndex >= 0 && inputIndex < kSourceClipCount);
@@ -1904,6 +2018,176 @@ SeExprPlugin::getRegionOfDefinition(const OFX::RegionOfDefinitionArguments &args
 }
 
 using namespace OFX;
+
+class SeExprInteract
+: public OFX::RectangleInteract
+{
+public:
+    
+    SeExprInteract(OfxInteractHandle handle,
+                      OFX::ImageEffect* effect);
+    
+    virtual bool draw(const OFX::DrawArgs &args) OVERRIDE FINAL;
+    virtual bool penMotion(const OFX::PenArgs &args) OVERRIDE FINAL;
+    virtual bool penDown(const OFX::PenArgs &args) OVERRIDE FINAL;
+    virtual bool penUp(const OFX::PenArgs &args) OVERRIDE FINAL;
+    virtual bool keyDown(const OFX::KeyArgs &args) OVERRIDE FINAL;
+    virtual bool keyUp(const OFX::KeyArgs & args) OVERRIDE FINAL;
+    virtual void loseFocus(const OFX::FocusArgs &args) OVERRIDE FINAL;
+    
+private:
+    
+    virtual void aboutToCheckInteractivity(OfxTime time) OVERRIDE FINAL;
+    virtual bool allowTopLeftInteraction() const OVERRIDE FINAL;
+    virtual bool allowBtmRightInteraction() const OVERRIDE FINAL;
+    virtual bool allowBtmLeftInteraction() const OVERRIDE FINAL;
+    virtual bool allowBtmMidInteraction() const OVERRIDE FINAL;
+    virtual bool allowMidLeftInteraction() const OVERRIDE FINAL;
+    virtual bool allowCenterInteraction() const OVERRIDE FINAL;
+    
+    OFX::ChoiceParam* _boundingBox;
+    int _bboxType;
+};
+
+SeExprInteract::SeExprInteract(OfxInteractHandle handle,
+                                     OFX::ImageEffect* effect)
+: RectangleInteract(handle,effect)
+, _boundingBox(0)
+, _bboxType(0)
+{
+    _boundingBox = effect->fetchChoiceParam(kParamRegionOfDefinition);
+    assert(_boundingBox);
+}
+
+void SeExprInteract::aboutToCheckInteractivity(OfxTime /*time*/)
+{
+    int type_i;
+    _boundingBox->getValue(type_i);
+    _bboxType = type_i;
+}
+
+bool SeExprInteract::allowTopLeftInteraction() const
+{
+    return _bboxType == 2;
+}
+
+bool SeExprInteract::allowBtmRightInteraction() const
+{
+    return _bboxType == 2;
+}
+
+bool SeExprInteract::allowBtmLeftInteraction() const
+{
+    return _bboxType == 2;
+}
+
+bool SeExprInteract::allowBtmMidInteraction() const
+{
+    return _bboxType == 2;
+}
+
+bool SeExprInteract::allowMidLeftInteraction() const
+{
+    return _bboxType == 2;
+}
+
+bool SeExprInteract::allowCenterInteraction() const
+{
+    return _bboxType == 2;
+}
+
+bool
+SeExprInteract::draw(const OFX::DrawArgs &args)
+{
+    int type_i;
+    _boundingBox->getValue(type_i);
+    
+    if (type_i != 2) {
+        return false;
+    }
+    
+    return RectangleInteract::draw(args);
+}
+
+bool
+SeExprInteract::penMotion(const OFX::PenArgs &args)
+{
+    int type_i;
+    _boundingBox->getValue(type_i);
+    
+    if (type_i != 2) {
+        return false;
+    }
+    
+    return RectangleInteract::penMotion(args);
+}
+
+bool
+SeExprInteract::penDown(const OFX::PenArgs &args)
+{
+    int type_i;
+    _boundingBox->getValue(type_i);
+    
+    if (type_i != 2) {
+        return false;
+    }
+    
+    return RectangleInteract::penDown(args);
+}
+
+bool
+SeExprInteract::penUp(const OFX::PenArgs &args)
+{
+    int type_i;
+    _boundingBox->getValue(type_i);
+    
+    if (type_i != 2) {
+        return false;
+    }
+    
+    return RectangleInteract::penUp(args);
+}
+
+void
+SeExprInteract::loseFocus(const OFX::FocusArgs &args)
+{
+    return RectangleInteract::loseFocus(args);
+}
+
+
+
+bool
+SeExprInteract::keyDown(const OFX::KeyArgs &args)
+{
+    int type_i;
+    _boundingBox->getValue(type_i);
+    
+    if (type_i != 2) {
+        return false;
+    }
+    
+    return RectangleInteract::keyDown(args);
+}
+
+bool
+SeExprInteract::keyUp(const OFX::KeyArgs & args)
+{
+    int type_i;
+    _boundingBox->getValue(type_i);
+    
+    if (type_i != 2) {
+        return false;
+    }
+    
+    return RectangleInteract::keyUp(args);
+}
+
+
+
+class SeExprOverlayDescriptor
+: public DefaultEffectOverlayDescriptor<SeExprOverlayDescriptor, SeExprInteract>
+{
+};
 
 mDeclarePluginFactory(SeExprPluginFactory, {}, {});
 
@@ -1949,6 +2233,8 @@ void SeExprPluginFactory::describe(OFX::ImageEffectDescriptor &desc)
 #else 
     gHostIsMultiPlanar = false;
 #endif
+    
+    desc.setOverlayInteractDescriptor(new SeExprOverlayDescriptor);
 }
 
 void SeExprPluginFactory::describeInContext(OFX::ImageEffectDescriptor &desc, OFX::ContextEnum context)
@@ -2000,14 +2286,106 @@ void SeExprPluginFactory::describeInContext(OFX::ImageEffectDescriptor &desc, OF
         ChoiceParamDescriptor* param = desc.defineChoiceParam(kParamRegionOfDefinition);
         param->setLabel(kParamRegionOfDefinitionLabel);
         param->setHint(kParamRegionOfDefinitionHint);
+        
         param->appendOption(kParamRegionOfDefinitionOptionUnion, kParamRegionOfDefinitionOptionUnionHelp);
         param->appendOption(kParamRegionOfDefinitionOptionIntersection, kParamRegionOfDefinitionOptionIntersectionHelp);
+        param->appendOption(kParamRegionOfDefinitionOptionSize, kParamRegionOfDefinitionOptionSizeHelp);
+        param->appendOption(kParamRegionOfDefinitionOptionFormat, kParamRegionOfDefinitionOptionFormatHelp);
+        param->appendOption(kParamRegionOfDefinitionOptionProject, kParamRegionOfDefinitionOptionProjectHelp);
+
         for (int i = 0; i < kSourceClipCount; ++i) {
             snprintf(name, sizeof(name), kParamRegionOfDefinitionOptionCustomInput, i+1);
             snprintf(help, sizeof(help), kParamRegionOfDefinitionOptionCustomInputHelp, i+1);
             param->appendOption(name, help);
         }
         param->setAnimates(false);
+        if (page) {
+            page->addChild(*param);
+        }
+    }
+    
+    {
+        ChoiceParamDescriptor* param = desc.defineChoiceParam(kParamGeneratorFormat);
+        param->setLabel(kParamGeneratorFormatLabel);
+        param->setAnimates(false);
+        assert(param->getNOptions() == eParamFormatPCVideo);
+        param->appendOption(kParamFormatPCVideoLabel);
+        assert(param->getNOptions() == eParamFormatNTSC);
+        param->appendOption(kParamFormatNTSCLabel);
+        assert(param->getNOptions() == eParamFormatPAL);
+        param->appendOption(kParamFormatPALLabel);
+        assert(param->getNOptions() == eParamFormatHD);
+        param->appendOption(kParamFormatHDLabel);
+        assert(param->getNOptions() == eParamFormatNTSC169);
+        param->appendOption(kParamFormatNTSC169Label);
+        assert(param->getNOptions() == eParamFormatPAL169);
+        param->appendOption(kParamFormatPAL169Label);
+        assert(param->getNOptions() == eParamFormat1kSuper35);
+        param->appendOption(kParamFormat1kSuper35Label);
+        assert(param->getNOptions() == eParamFormat1kCinemascope);
+        param->appendOption(kParamFormat1kCinemascopeLabel);
+        assert(param->getNOptions() == eParamFormat2kSuper35);
+        param->appendOption(kParamFormat2kSuper35Label);
+        assert(param->getNOptions() == eParamFormat2kCinemascope);
+        param->appendOption(kParamFormat2kCinemascopeLabel);
+        assert(param->getNOptions() == eParamFormat4kSuper35);
+        param->appendOption(kParamFormat4kSuper35Label);
+        assert(param->getNOptions() == eParamFormat4kCinemascope);
+        param->appendOption(kParamFormat4kCinemascopeLabel);
+        assert(param->getNOptions() == eParamFormatSquare256);
+        param->appendOption(kParamFormatSquare256Label);
+        assert(param->getNOptions() == eParamFormatSquare512);
+        param->appendOption(kParamFormatSquare512Label);
+        assert(param->getNOptions() == eParamFormatSquare1k);
+        param->appendOption(kParamFormatSquare1kLabel);
+        assert(param->getNOptions() == eParamFormatSquare2k);
+        param->appendOption(kParamFormatSquare2kLabel);
+        param->setDefault(0);
+        param->setHint(kParamGeneratorFormatHint);
+        desc.addClipPreferencesSlaveParam(*param);
+        if (page) {
+            page->addChild(*param);
+        }
+    }
+    
+    // btmLeft
+    {
+        Double2DParamDescriptor* param = desc.defineDouble2DParam(kParamRectangleInteractBtmLeft);
+        param->setLabel(kParamRectangleInteractBtmLeftLabel);
+        param->setDoubleType(OFX::eDoubleTypeXYAbsolute);
+        param->setDefaultCoordinateSystem(OFX::eCoordinatesNormalised);
+        param->setDefault(0., 0.);
+        param->setIncrement(1.);
+        param->setHint("Coordinates of the bottom left corner of the size rectangle.");
+        param->setDigits(0);
+        if (page) {
+            page->addChild(*param);
+        }
+    }
+    
+    // size
+    {
+        Double2DParamDescriptor* param = desc.defineDouble2DParam(kParamRectangleInteractSize);
+        param->setLabel(kParamRectangleInteractSizeLabel);
+        param->setDoubleType(OFX::eDoubleTypeXY);
+        param->setDefaultCoordinateSystem(OFX::eCoordinatesNormalised);
+        param->setDefault(1., 1.);
+        param->setIncrement(1.);
+        param->setDimensionLabels(kParamRectangleInteractSizeDim1, kParamRectangleInteractSizeDim1);
+        param->setHint("Width and height of the size rectangle.");
+        param->setIncrement(1.);
+        param->setDigits(0);
+        if (page) {
+            page->addChild(*param);
+        }
+    }
+
+    // interactive
+    {
+        BooleanParamDescriptor* param = desc.defineBooleanParam(kParamRectangleInteractInteractive);
+        param->setLabel(kParamRectangleInteractInteractiveLabel);
+        param->setHint(kParamRectangleInteractInteractiveHint);
+        param->setEvaluateOnChange(false);
         if (page) {
             page->addChild(*param);
         }
