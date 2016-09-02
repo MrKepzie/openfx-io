b68e7d78e843eab335b145f31764ab7af6f7c422
Frédéric Devernay <frederic.devernay@m4x.org>
2016-09-02 16:25:28 +0200

    fix build

--- RunScript/RunScript.cpp
+++ RunScript/RunScript.cpp
@@ -40,7 +40,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-//#include <stdio.h> // for snprintf & _snprintf
+#include <stdio.h> // for snprintf & _snprintf
 #if defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
 #  include <windows.h>
 #  if defined(_MSC_VER) && _MSC_VER < 1900
