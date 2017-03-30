MrKepzie/openfx-io

commit 62eeaca21a445a585f6e49cd635c5631a277e41c

GenericReader:  add inputSpaceSet param
see https://github.com/MrKepzie/Natron/issues/1492

IOSupport/GenericReader.cpp
@@ -272,6 +272,7 @@ GenericReaderPlugin::GenericReaderPlugin(OfxImageEffectHandle handle,
     : ImageEffect(handle)
     , _missingFrameParam(0)
 #ifdef OFX_IO_USING_OCIO
+    , _inputSpaceSet(NULL)
     , _ocio( new GenericOCIO(this) )
 #endif
     , _outputClip(0)
@@ -332,6 +333,9 @@ GenericReaderPlugin::GenericReaderPlugin(OfxImageEffectHandle handle,
     }
     _guessedParams = fetchBooleanParam(kParamGuessedParams);
 
+#ifdef OFX_IO_USING_OCIO
+    _inputSpaceSet = fetchBooleanParam(kParamInputSpaceSet);
+#endif
 
     // must be in sync with GenericReaderDescribeInContextBegin
     int i = 0;
@@ -2034,7 +2038,12 @@ GenericReaderPlugin::changedFilename(const InstanceChangedArgs &args)
             return;
         }
 
+        bool setColorSpace = true;
 #ifdef OFX_IO_USING_OCIO
+        // if inputSpaceSet == true (output space was manually set by user) then setColorSpace = false
+        if ( _inputSpaceSet->getValue() ) {
+            setColorSpace = false;
+        }
         // Colorspace parsed from filename overrides the file colorspace,
         // following recommendations from http://opencolorio.org/configurations/spi_pipeline.html
         // However, as discussed in https://groups.google.com/forum/#!topic/ocio-dev/dfOxq8Nanl8
@@ -2043,7 +2052,7 @@ GenericReaderPlugin::changedFilename(const InstanceChangedArgs &args)
         // https://github.com/imageworks/OpenColorIO/pull/381 or
         // https://github.com/imageworks/OpenColorIO/pull/413 may be merged.
         OCIO::ConstConfigRcPtr ocioConfig = _ocio->getConfig();
-        if (ocioConfig) {
+        if (setColorSpace && ocioConfig) {
             string name = filename;
             (void)SequenceParsing::removePath(name); // only use the file NAME
             const char* colorSpaceStr = ocioConfig->parseColorSpaceFromString( name.c_str() );
@@ -2077,10 +2086,10 @@ GenericReaderPlugin::changedFilename(const InstanceChangedArgs &args)
             if (colorSpaceStr) {
                 assert( _ocio->hasColorspace(colorSpaceStr) ); // parseColorSpaceFromString always returns an existing colorspace
                 // we're lucky
-                colorspace = colorSpaceStr;
+                _ocio->setInputColorspace( colorspace.c_str() );
+                setColorSpace = false;
             }
         }
-        _ocio->setInputColorspace( colorspace.c_str() );
 
         // Refreshing the choice menus of ocio is required since the instanceChanged action
         // may not be called recursively during the createInstance action
@@ -2294,6 +2303,12 @@ GenericReaderPlugin::changedParam(const InstanceChangedArgs &args,
                 }
             }
         }
+#ifdef OFX_IO_USING_OCIO
+    } else if ( ( (paramName == kOCIOParamInputSpace) || (paramName == kOCIOParamInputSpaceChoice) ) &&
+               ( args.reason == eChangeUserEdit) ) {
+        // set the inputSpaceSet param to true https://github.com/MrKepzie/Natron/issues/1492
+        _inputSpaceSet->setValue(true);
+#endif
     }
 
 #ifdef OFX_IO_USING_OCIO

IOSupport/GenericReader.h
@@ -387,6 +387,7 @@ private:
 
 protected:
 #ifdef OFX_IO_USING_OCIO
+    OFX::BooleanParam* _inputSpaceSet;
     std::auto_ptr<GenericOCIO> _ocio;
 #endif
 

commit b309211348b1e59fb1086380fc3aa84cb3481e4d

GenericReader: fix bug introduced in 62eeaca21a445a585f6e49cd635c5631a277e41c
colorspace was not set from the guessed colorspace

IOSupport/GenericReader.cpp
@@ -2040,7 +2040,7 @@ GenericReaderPlugin::changedFilename(const InstanceChangedArgs &args)
 
         bool setColorSpace = true;
 #ifdef OFX_IO_USING_OCIO
-        // if inputSpaceSet == true (output space was manually set by user) then setColorSpace = false
+        // if inputSpaceSet == true (input space was manually set by user) then setColorSpace = false
         if ( _inputSpaceSet->getValue() ) {
             setColorSpace = false;
         }
@@ -2086,8 +2086,11 @@ GenericReaderPlugin::changedFilename(const InstanceChangedArgs &args)
             if (colorSpaceStr) {
                 assert( _ocio->hasColorspace(colorSpaceStr) ); // parseColorSpaceFromString always returns an existing colorspace
                 // we're lucky
-                _ocio->setInputColorspace( colorspace.c_str() );
+                _ocio->setInputColorspace(colorSpaceStr);
                 setColorSpace = false;
+            }
+            if (setColorSpace) {
+                _ocio->setInputColorspace( colorspace.c_str() );
             }
         }
 